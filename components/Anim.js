b798e59a4d67855ca54e80dee6cc179b6ce04c45
Your name <Your email>
2024-08-22 23:25:51 +0200

    debugok

--- components/Anim.js
+++ components/Anim.js
@@ -10,7 +10,6 @@ function Anim() {
         scene="https://prod.spline.design/5wAMHQgZEspSGyGg/scene.splinecode"
         width={1123}
         height={905}
-        className="   border-slate-200 bg rounded-full"
       />
     </div>
   );
