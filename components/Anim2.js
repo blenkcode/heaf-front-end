4806460f023f7ebb89422077fa7be131455957b0
Your name <Your email>
2024-08-22 18:18:04 +0200

    debug

--- components/Anim2.js
+++ components/Anim2.js
@@ -3,11 +3,11 @@ import Spline from "@splinetool/react-spline";
 export default function Anim2() {
   return (
     <main className="">
-      <Spline
+      {/* <Spline
         scene="https://prod.spline.design/2iAbeMFCYX4GCLll/scene.splinecode"
         width={132}
         height={229}
-      />
+      /> */}
     </main>
   );
 }
