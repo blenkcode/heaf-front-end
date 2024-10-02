04514bb3a607aa23849e5e15b10007f64768e9eb
Your name <valentinmor.pro@gmail.com>
2024-10-02 15:20:00 +0200

    maj

--- components/Home.js
+++ components/Home.js
@@ -7,6 +7,7 @@ import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
 import Loader from "./Loader"; // Importer le composant Loader
 import React from "react";
 import Header from "./Header";
+
 function Home({}) {
   const [currentComponent, setCurrentComponent] = useState("SignUp");
   const [isLoading, setIsLoading] = useState(true);
@@ -31,26 +32,28 @@ function Home({}) {
   return (
     <>
       {isLoading && <Loader />}
-      {/* Le composant Anim est rendu même pendant le chargement, mais la visibilité est contrôlée */}
       <div
         className={`transition-opacity duration-500 ${
           isLoading ? "opacity-0" : "opacity-100"
         }`}
       >
-        <main className="h-fit lg:h-lvh text-sky-900 bg-diagonal-split flex flex-col xl:flex-row justify-evenly items-center relative overflow-x-hidden ">
-          <Header></Header>
-          <div className="flex flex-col sm:flex-row xl:flex-col relative xl:items-start items-center xl-w-fit justify-around z-10 xl:ml-10 ml-0 2xl:ml-0 pt-24 sm:pt-12 pt-10 xl:pt-0 ">
-            <div className="xl:w-full w-full sm:w-1/2 px-10 lg:px-0">
-              <h1 className="lg:text-7xl text-3xl flex items-center font-bold">
+        {/* Utilisation de Flexbox pour centrer verticalement et horizontalement */}
+        <main className="min-h-screen  flex flex-col xl:flex-row justify-center items-center text-sky-900 bg-diagonal-split relative overflow-x-hidden">
+          <Header />
+
+          <div className="flex flex-col sm:flex-row  relative items-center justify-around z-10 pt-24 sm:pt-12 xl:pt-0  2xl:w-1/2 xl:w-2/3 lg:w-4/5">
+            <div className="xl:w-full relative w-full sm:w-1/2 px-10 lg:px-0 text-center xl:text-left ">
+              <div className="w-144 h-144 lg:visible invisible absolute -top-32 -left-32 rounded-full border-8 border-sky-900 z-10"></div>
+              <h1 className="lg:text-7xl text-3xl flex items-center justify-center lg:justify-start font-bold">
                 Heaf{" "}
                 <a
                   href="https://www.valentin-mor.com/"
-                  className=" border-1 border-opacity-55 px-6 py-3 lg:text-xl text-base rounded-xl border-sky-900 cursor-pointer hover:bg-sky-900 hover:border-slate-200 hover:text-slate-200  text-base transition-colors ml-10"
+                  className=" border-1 border-opacity-55 px-6 py-3 lg:text-xl text-base rounded-xl border-sky-900 cursor-pointer hover:bg-sky-900 hover:border-slate-200 hover:text-slate-200 text-base transition-colors ml-10 z-50"
                 >
                   Contact
                 </a>
               </h1>
-              <h2 className="lg:text-2xl sm:text-base md:text-xl text-sm  2xl:text4xl lg:mt-10 mt-5 ">
+              <h2 className="lg:text-xl sm:text-base md:text-xl text-sm   lg:mt-10 mt-5">
                 <FontAwesomeIcon icon={faArrowRight} className="lg:pr-5 pr-2" />
                 Calculez vos besoins nutritionnels
                 <div className="lg:mt-10 mt-5">
@@ -70,7 +73,7 @@ function Home({}) {
                 </div>
               </h2>
             </div>
-            <div className="z-50 flex pt-10 sm:pt-0 xl:pt-10 sm:ml-10 xl:ml-0">
+            <div className="z-50 flex pt-10 sm:pt-0 xl:pt-10 sm:ml-10 xl:ml-0 ">
               {currentComponent === "SignUp" ? (
                 <SignUp onToggleForm={toggleForm} />
               ) : (
@@ -78,9 +81,9 @@ function Home({}) {
               )}
             </div>
           </div>
-          <div className="w-fit h-fit z-20">
+          {/* <div className="w-fit h-fit z-20">
             <Anim onLoad={handleSplineLoad} />
-          </div>
+          </div> */}
         </main>
       </div>
     </>
--- pages/form.js
+++ pages/form.js
@@ -81,127 +81,128 @@ function Form({}) {
   const pseudo = useSelector((state) => state.user.value.pseudo);
   console.log(pseudo);
   return (
-    <main className="h-auto min-h-lvh text-sky-900 bg-diagonal-split  flex flex-col xl:flex-row justify-evenly items-center relative overflow-x-hidden ">
+    <main className="h-auto min-h-lvh text-sky-900 bg-diagonal-split  flex flex-col  justify-evenly items-center relative overflow-x-hidden ">
       <Header></Header>
-      <div className=" w-full xl:w-1/2 h-full px-5 lg:pl-32 pt-12 xl:pt-0 ">
-        <div className="w-full mb-12 2xl:mt-0 lg:mt-16 mt-10 h-auto flex flex-col justify-center items-start pt-0 2xl:pb-10  ">
+      <div className="  z-10 w-full flex-col flex items-center  justify-center h-full px-5 pt-12 lg:pt-0  ">
+        <div className="w-fit relative lg:mb-0 mb-12 2xl:mt-0 lg:mt-16  mt-10 h-auto flex flex-col justify-center items-start pt-0   ">
+          <div className="w-96 h-160 lg:visible invisible absolute -top-20 -left-24 rounded-full border-8 border-sky-900 z-10 "></div>
           <div className="lg:text-6xl text-3xl font-bold ">
             Hello{" "}
             <span className="lg:text-6xl text-3xl font-bold ">{pseudo}</span>!
           </div>
           <br />
-          <div className="lg:text-2xl text-lg font-normal ">
+          <div className="lg:text-2xl text-lg font-normal z-50 lg:py-5 bg-diagonal-split ">
             {" "}
             Ces informations nous permettent de définir ensemble ton profil
             Heaf.
           </div>
-        </div>
-
-        <div className="flex flex-col 2xl:flex-row xl:flex-col lg:flex-row w-full ">
-          {" "}
-          <div className=" lg:w-fit h-auto flex flex-col justify-center lg:items-start items-center">
-            <div className="lg:w-80 w-full ">
-              <div className=" flex justify-between items-start mb-6  ">
-                <span className="lg:text-xl text-base ">Âge :</span>
-                <input
-                  value={age}
-                  onChange={handleAgeChange}
-                  className="px-2 py-2 rounded-xl bg-slate-100 bg-opacity-0  placeholder-sky-900  border-1 border-sky-900  border-opacity-90"
-                  type="text"
-                />
-              </div>
-              <div className=" flex justify-between items-start mb-6">
-                <span className="lg:text-xl text-base ">Poids (Kgs)</span>
-                <input
-                  value={weight}
-                  onChange={handleWeightChange}
-                  className="px-2 py-2 rounded-xl bg-slate-100 bg-opacity-0  placeholder-sky-900  border-1 border-sky-900  border-opacity-90"
-                  type="text"
-                />
-              </div>
-              <div className=" flex justify-between items-start mb-6">
-                <span className="lg:text-xl text-base ">Taille (cm)</span>
-                <input
-                  value={height}
-                  onChange={handleHeightChange}
-                  className="px-2 py-2 rounded-xl bg-slate-100 bg-opacity-0  placeholder-sky-900  border-1 border-sky-900  border-opacity-90"
-                  type="text"
-                />
-              </div>
-              <div className=" flex justify-between items-center mb-6">
-                <span className="lg:text-xl text-base ">Sexe : </span>
-                <div className="flex items-center justify-center mt-1">
-                  <div className=" ">
-                    <input
-                      className=""
-                      type="radio"
-                      name="gender"
-                      value="female"
-                      checked={gender === "female"}
-                      onChange={handleGenderChange}
-                    />
-                    <label className="ml-2 text-lg ">Femme</label>
+          <div className="flex flex-col justify-between lg:flex-row mt-10 lg:mt-5   bg-diagonal-split w-3/4 z-50">
+            <div className=" lg:w-fit h-auto flex flex-col justify-center lg:items-start items-center bg-diagonal-split  ">
+              <div className="lg:w-80 w-full ">
+                <div className=" flex justify-between items-center lg:mb-3 mb-6  ">
+                  <span className="lg:text-xl text-base ">Âge :</span>
+                  <input
+                    value={age}
+                    onChange={handleAgeChange}
+                    className="px-2 py-2 rounded-xl bg-slate-100 bg-opacity-0  placeholder-sky-900  border-1 border-sky-900  border-opacity-90"
+                    type="text"
+                  />
+                </div>
+                <div className=" flex justify-between items-center lg:mb-3 mb-6  ">
+                  <span className="lg:text-xl text-base ">Poids (Kgs)</span>
+                  <input
+                    value={weight}
+                    onChange={handleWeightChange}
+                    className="px-2 py-2 rounded-xl bg-slate-100 bg-opacity-0  placeholder-sky-900  border-1 border-sky-900  border-opacity-90"
+                    type="text"
+                  />
+                </div>
+                <div className=" flex justify-between items-center lg:mb-3 mb-6  ">
+                  <span className="lg:text-xl text-base mr-5">Taille (cm)</span>
+                  <input
+                    value={height}
+                    onChange={handleHeightChange}
+                    className="px-2 py-2 rounded-xl bg-slate-100 bg-opacity-0  placeholder-sky-900  border-1 border-sky-900  border-opacity-90"
+                    type="text"
+                  />
+                </div>
+                <div className=" flex justify-between items-center lg:mb-3 mb-6  ">
+                  <span className="lg:text-xl text-base ">Sexe : </span>
+                  <div className="flex items-center justify-center mt-1">
+                    <div className=" ">
+                      <input
+                        className=""
+                        type="radio"
+                        name="gender"
+                        value="female"
+                        checked={gender === "female"}
+                        onChange={handleGenderChange}
+                      />
+                      <label className="ml-2 text-lg ">Femme</label>
+                    </div>
+                    <div className="ml-6">
+                      <input
+                        className=" "
+                        type="radio"
+                        name="gender"
+                        value="male"
+                        checked={gender === "male"}
+                        onChange={handleGenderChange}
+                      />
+                      <label className="ml-2 text-lg ">Homme</label>
+                    </div>
                   </div>
-                  <div className="ml-6">
-                    <input
-                      className=" "
-                      type="radio"
-                      name="gender"
-                      value="male"
-                      checked={gender === "male"}
-                      onChange={handleGenderChange}
-                    />
-                    <label className="ml-2 text-lg ">Homme</label>
+                </div>
+              </div>
+            </div>
+            <div className="flex flex-col lg:flex-row w-fit lg:ml-10  ">
+              {" "}
+              <div className=" flex flex-col items-start justify-start  w-full z-50 bg-diagonal-split rounded-xl">
+                {" "}
+                <div className="flex items-start flex-col w-full  lg-mt-0">
+                  <label className="lg:text-xl text-md lg:mb-1 mb-5 ">
+                    Niveau d'activité :
+                  </label>
+                  <select
+                    className="form-select mt-2 rounded-xl py-2 px-4 lg:w-40 w-32"
+                    name="activity"
+                    value={activityLevel}
+                    onChange={handleActivityLevelChange}
+                  >
+                    <option value="1.2">Sédentaire</option>
+                    <option value="1.5">Modérément actif</option>
+                    <option value="1.6">Actif</option>
+                    <option value="1.8">Extrêmement actif</option>
+                  </select>
+                </div>
+                <div className="w-full  2xl:mt-10 mt-10 ">
+                  <span className="lg:text-xl text-md lg:mb-1 mb-5">
+                    Objectif :{" "}
+                  </span>
+                  <div className="lg:mt-0 mt-5">
+                    <select
+                      className="form-select mt-2 rounded-xl py-2 px-4 lg:w-40 w-32"
+                      name="caloriesDeficit"
+                      value={caloriesDeficit}
+                      onChange={handleObjectifChange}
+                    >
+                      <option value="-500">Perte de poids</option>
+                      <option value="500">Prise de muscle</option>
+                      <option value="0">Maintien</option>
+                    </select>
                   </div>
                 </div>
               </div>
             </div>
           </div>
-          <div className=" flex 2xl:flex-col items-start justify-start 2xl:pl-24 pl-0 xl:mt-0 lg:ml-24 xl:ml-0 lg:w-96 w-full ">
-            {" "}
-            <div className="flex items-start flex-col w-full">
-              <label className="lg:text-xl text-md mb-5">
-                Niveau d'activité :
-              </label>
-              <select
-                className="form-select mt-2 rounded-xl py-2 px-4 lg:w-40 w-32"
-                name="activity"
-                value={activityLevel}
-                onChange={handleActivityLevelChange}
-              >
-                <option value="1.2">Sédentaire</option>
-                <option value="1.5">Modérément actif</option>
-                <option value="1.6">Actif</option>
-                <option value="1.8">Extrêmement actif</option>
-              </select>
-            </div>
-            <div className="w-full  2xl:mt-10 ">
-              <span className="lg:text-xl text-md mb-5">Objectif : </span>
-              <div className="mt-5">
-                <select
-                  className="form-select mt-2 rounded-xl py-2 px-4 lg:w-40 w-32"
-                  name="caloriesDeficit"
-                  value={caloriesDeficit}
-                  onChange={handleObjectifChange}
-                >
-                  <option value="-500">Perte de poids</option>
-                  <option value="500">Prise de muscle</option>
-                  <option value="0">Maintien</option>
-                </select>
-              </div>
-            </div>
-          </div>
         </div>
+
         <button
           onClick={() => handleSubmit()}
           className="px-8 py-2 mt-10 rounded-xl bg-opacity-80 text-sky-900  border-1 border-sky-900 border-opacity-90 hover:bg-sky-900 hover:border-slate-200 hover:text-slate-200 transition-colors "
         >
           Suivant
         </button>
-      </div>
-      <div className="w-auto h-fit z-20">
-        {/* z-index élevé pour Anim */}
-        <Anim />
       </div>
     </main>
   );
